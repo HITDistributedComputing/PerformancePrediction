5513e8bd59d709f985acf5276880722ee126fd6e
vzeng-q470 <yukun.zeng.hit@outlook.com>
2016-05-30 17:56:38 +0800

    added comments in exception package

--- src/cn/hit/cst/ssl/exception/NullModelException.java
+++ src/cn/hit/cst/ssl/exception/NullModelException.java
@@ -1,5 +1,12 @@
 package cn.hit.cst.ssl.exception;
-
+/**
+ * 
+* @ClassName: NullModelException 
+* @Description: the exception for throwing when the prediction model hasn't been initialized
+* @author Yukun Zeng
+* @date May 30, 2016 5:55:03 PM 
+*
+ */
 public class NullModelException extends Exception {
 	public NullModelException(String msg){
 		super(msg + ": " + "The model for prediction hasn't been initialized.");
--- src/cn/hit/cst/ssl/exception/XYArraySizeException.java
+++ src/cn/hit/cst/ssl/exception/XYArraySizeException.java
@@ -1,5 +1,12 @@
 package cn.hit.cst.ssl.exception;
-
+/**
+ * 
+* @ClassName: XYArraySizeException 
+* @Description: the exception for throwing when x y array share different sizes
+* @author Yukun Zeng
+* @date May 30, 2016 5:55:58 PM 
+*
+ */
 public class XYArraySizeException extends Exception {
 	public XYArraySizeException(String msg){
 		super(msg + ": X Y ArrayList elements count unequal.");
